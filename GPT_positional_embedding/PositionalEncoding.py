05c030eeb2ba4606ac8bb0ac06ca859515a76eb4
haniOrwhanhee <ghksgml1919@gmail.com>
2021-01-20 06:33:05 +0900

    update pickle dataset and positional embeds before training

---
+++ GPT_positional_embedding/PositionalEncoding.py
@@ -0,0 +1,45 @@
+import torch
+import numpy as np
+
+
+def get_positional_embedding(max_len, dim=768):
+    pe = torch.Tensor(max_len, dim)
+
+    for i in range(max_len):
+        pe[i, :] = (i + 1)
+
+    return pe
+
+
+def get_binary_embedding(max_len, dim):
+    pe = torch.Tensor(max_len, dim)
+    bin_form = '{0:' + str(max_len) + 'b}'
+
+    for i in range(max_len):
+        bin_form.format(i)
+        pe[i, :] = int(bin_form)
+
+    return pe
+
+
+def get_sinusoidal_embedding(max_len, dim=768):
+    pe = torch.Tensor(max_len, dim)
+
+    pos = torch.arange(0, max_len, 1.).unsqueeze(1)
+    k = torch.exp(-np.log(10000) * torch.arange(0, dim, 2.) / dim)
+
+    pe[:, 0::2] = torch.sin(pos * k)
+    pe[:, 1::2] = torch.cos(pos * k)
+
+    return pe
+
+def get_sinusoidal_embedding(max_len, dim=768):
+    pe = torch.Tensor(max_len, dim)
+
+    pos = torch.arange(-1., max_len, 1.).unsqueeze(1)
+    k = torch.exp(-np.log(10000) * torch.arange(0, dim, 2.) / dim)
+
+    pe[:, 0::2] = torch.sin(pos * k)
+    pe[:, 1::2] = torch.cos(pos * k)
+
+    return pe
